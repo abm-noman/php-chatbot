2f8434ee68d46e33bc69e5a911990db33257700d
A. B. M. Noman <noman15-2839@diu.edu.bd>
2022-11-04 18:28:30 +0600

    Database Connected

---
+++ index.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Simple Chatbot in PHP</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Nunito&display=swap" rel="stylesheet">
+    <script src='https://kit.fontawesome.com/a076d05399.js' crossorigin='anonymous'></script>
+    <script src="https://code.jquery.com/jquery-3.6.1.min.js"></script>
+</head>
+<body>
+    <div class="wrapper">
+        <div class="title">Simple Online Chatbot</div>
+        <div class="form">
+            <div class="bot-inbox inbox">
+                <div class="icon">
+                    <i class="fas fa-user"></i>
+                </div>
+                <div class="msg-header">
+                    <p>Hello there. How can I help you?</p>
+                </div>
+            </div>
+        </div>
+        <div class="typing-field">
+            <div class="input-data">
+                <input id="data" type="text" placeholder="Type Something Here..." required>
+                <button id="send-btn">Send</button>
+            </div>
+        </div>
+    </div>
+
+    <script>
+        $(document).ready(function(){
+            $("#send-btn").on("click", function(){
+                $value = $("#data").val();
+                $msg = '<div class="user-inbox inbox"><div class="msg-header"><p>' + $value + '</p></div></div>';
+                $(".form").append($msg);
+                $("#data").val('');
+
+                // Start Ajax coding
+                $.ajax({
+                    url: 'message.php',
+                    type: 'POST',
+                    data: 'text ='+$value,
+                    success: function(result){
+                        $reply = '<div class="bot-inbox inbox"><div class="icon"><i class="fas fa-user"></i></div><div class="msg-header"><p>'+ result +'</p></div></div>';
+                        $(".form").append($reply);
+                    }
+                });
+            });
+        });
+    </script>
+</body>
+</html>
---
+++ message.php
@@ -0,0 +1,21 @@
+<?php
+// Connecting to Database
+
+$conn = mysqli_connect("localhost", "root", "", "chatbot" ) or die("Database Error");
+
+//Getting User Message through ajax
+$getMsg = mysqli_real_escape_string($conn, $_POST['text']);
+
+//Checking User query to Database Query
+$check_data = "SELECT replies FROM bot WHERE queries LIKE '%$setMsg%'";
+$run_query = mysqli_query($conn, $check_data) or die("Error");
+
+
+// If user query matches Database Queries, We will show Replies
+if(mysqli_num_rows($run_query) > 0){
+
+}else{
+    echo "Sorry! I can't get you.";
+}
+
+?>
